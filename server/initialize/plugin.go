d2838934b317c6e01363c05f2db745fa1ee3cc19
aaronlyc <blockchain_lyc@163.com>
2023-06-26 22:22:17 +0800

    更新插件示例

--- server/initialize/plugin.go
+++ server/initialize/plugin.go
@@ -3,9 +3,9 @@ package initialize
 import (
 	"fmt"
 
-	"appnest/server/global"
+	// "appnest/server/global"
 	"appnest/server/middleware"
-	"appnest/server/plugin/email"
+	// "appnest/server/plugin/email"
 	"appnest/server/utils/plugin"
 
 	"github.com/gin-gonic/gin"
@@ -25,13 +25,13 @@ func InstallPlugin(Router *gin.Engine) {
 	fmt.Printf("鉴权插件安装==》%#v\n", PrivateGroup)
 	PrivateGroup.Use(middleware.JWTAuth()).Use(middleware.CasbinHandler())
 	//  添加跟角色挂钩权限的插件 示例 本地示例模式于在线仓库模式注意上方的import 可以自行切换 效果相同
-	PluginInit(PrivateGroup, email.CreateEmailPlug(
-		global.GVA_CONFIG.Email.To,
-		global.GVA_CONFIG.Email.From,
-		global.GVA_CONFIG.Email.Host,
-		global.GVA_CONFIG.Email.Secret,
-		global.GVA_CONFIG.Email.Nickname,
-		global.GVA_CONFIG.Email.Port,
-		global.GVA_CONFIG.Email.IsSSL,
-	))
+	// PluginInit(PrivateGroup, email.CreateEmailPlug(
+	// 	global.GVA_CONFIG.Email.To,
+	// 	global.GVA_CONFIG.Email.From,
+	// 	global.GVA_CONFIG.Email.Host,
+	// 	global.GVA_CONFIG.Email.Secret,
+	// 	global.GVA_CONFIG.Email.Nickname,
+	// 	global.GVA_CONFIG.Email.Port,
+	// 	global.GVA_CONFIG.Email.IsSSL,
+	// ))
 }
